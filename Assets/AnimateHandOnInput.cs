26b6ebd84a6fc77b9786fdc3dfe99416125e6d25
Cagri Demir <dcagriburak@tocafootball.com>
2022-09-21 17:07:40 +0300

    HandsAdded
    Hands and interaction tool kit set up accordingly

---
+++ Assets/AnimateHandOnInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+        Debug.Log(triggerValue);
+        float gripValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+        
+    }
+}
